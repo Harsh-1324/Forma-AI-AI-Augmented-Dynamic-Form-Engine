4a56b9794841d26e28d1def9902e8606eeee225b
Akhil Alex <akhil462315@gmail.com>
2026-07-20 20:38:50 +0530

    Add login page UI with email/password validation, fix Tailwind v4 config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/common/Navbar.jsx";
 import FormListPage from "./routes/FormListPage.jsx";
 import FormFillPage from "./routes/FormFillPage.jsx";
 import ResumeFormPage from "./routes/ResumeFormPage.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <main className="app-main">
         <Routes>
           <Route path="/" element={<FormListPage />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/forms/:schemaId" element={<FormFillPage />} />
           <Route path="/submissions/:submissionId" element={<ResumeFormPage />} />
         </Routes>
