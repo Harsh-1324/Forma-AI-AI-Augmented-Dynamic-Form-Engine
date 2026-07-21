1fd2819af11831a44bae623eb6acbff83fa6c10b
Akhil Alex <akhil462315@gmail.com>
2026-07-22 01:33:06 +0530

    Add Signup page with validation, wire up /signup route, fix Get started link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import FormListPage from "./routes/FormListPage.jsx";
 import FormFillPage from "./routes/FormFillPage.jsx";
 import ResumeFormPage from "./routes/ResumeFormPage.jsx";
 import LoginPage from "./routes/LoginPage.jsx";
+import SignupPage from "./routes/SignupPage.jsx";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<FormListPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
           <Route path="/forms/:schemaId" element={<FormFillPage />} />
           <Route path="/submissions/:submissionId" element={<ResumeFormPage />} />
         </Routes>
